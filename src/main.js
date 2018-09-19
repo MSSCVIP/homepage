e17be2a40131bf9782adc5162a662022c5b760ac
cheesekon <cheesekon@cheesekondeMacBook-Pro.local>
2018-09-19 09:47:25 +0800

    init commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
+
 import router from './router'
+
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
